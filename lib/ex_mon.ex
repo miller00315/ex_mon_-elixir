9fe38c730c7e6013f688560516e902a9e62c1b02
Miller Cesar de Oliveira <miller00315@gmail.com>
2020-12-06 18:35:24 -0300

    Ajustando game over

--- lib/ex_mon.ex
+++ lib/ex_mon.ex
@@ -6,6 +6,7 @@ defmodule ExMon do
   alias ExMon.Game.Actions
 
   @computer_name "Robo"
+  @computer_moves [:move_avg, :move_rnd, :move_heal]
 
   def create_player(move_avg, move_heal, move_rnd, name),
     do: Player.build(move_avg, move_heal, move_rnd, name)
@@ -18,10 +19,20 @@ defmodule ExMon do
     Status.print_round_message(Game.info())
   end
 
-  def make_move(move) do
+  def make_move(move),
+    do:
+      Game.info()
+      |> Map.get(:status)
+      |> handle_status(move)
+
+  defp handle_status(:game_over, _move), do: Status.print_round_message(Game.info())
+
+  defp handle_status(_other, move) do
     move
     |> Actions.fetch_move()
     |> do_move()
+
+    computer_move(Game.info())
   end
 
   def info(), do: Game.info()
@@ -36,4 +47,11 @@ defmodule ExMon do
 
     Status.print_round_message(Game.info())
   end
+
+  defp computer_move(%{turn: :computer, status: :continue}) do
+    move = {:ok, Enum.random(@computer_moves)}
+    do_move(move)
+  end
+
+  defp computer_move(_), do: :ok
 end
